af2040fc4edb44a168dbae3b64523a4037bfe4d4
Andrew Brookins <andrew.brookins@redislabs.com>
2020-06-08 22:56:41 +0000

    Add test for challenge #5

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -41,22 +41,33 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
 
         return {FlatSiteSchema().load(self.redis.hgetall(hash_key)) for hash_key in sites}
 
+    # Challenge #5
+    def _find_by_geo_with_capacity_noop(self, query: GeoQuery) -> Set[Site]:
+        return {}
+
     def _find_by_geo_with_capacity(self, query: GeoQuery, **kwargs) -> Set[Site]:
         # START Challenge #5
+        # TODO: Get the sites matching the GEO query.
         coord = query.coordinate
-        radius_response = self.redis.georadius(  # type: ignore
+        radius_responses = self.redis.georadius(  # type: ignore
             self.key_schema.site_geo_key(), coord.lng, coord.lat, query.radius,
             query.radius_unit.value)
         # END Challenge #5
 
         sites = {
             FlatSiteSchema().load(self.redis.hgetall(hash_key))
-            for hash_key in radius_response
+            for hash_key in radius_responses
         }
         site_ids = [site.id for site in sites]
+        p = self.redis.pipeline(transaction=False)
 
         # START Challenge #5
-        p = self.redis.pipeline(transaction=False)
+        #
+        # TODO: Populate a dictionary called "scores" whose keys are site IDs
+        # and whose values are the site's capacity.
+        #
+        # Make sure to run any Redis commands against the Pipeline object
+        # "p" for better performance.
         for site in sites:
             p.zscore(self.key_schema.capacity_ranking_key(), site.id)
         scores = dict(zip(site_ids, reversed(p.execute())))
@@ -70,7 +81,7 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
     def find_by_geo(self, query: GeoQuery, **kwargs) -> Set[Site]:
         """Find Sites using a geographic query."""
         if query.only_excess_capacity:
-            return self._find_by_geo_with_capacity(query)
+            return self._find_by_geo_with_capacity_noop(query)
         return self._find_by_geo(query)
 
     def find_all(self, **kwargs) -> Set[Site]:
--- tests/dao/redis/test_site_geo.py
+++ tests/dao/redis/test_site_geo.py
@@ -1,9 +1,13 @@
+import datetime
+
 import pytest
 
 from redisolar.dao.redis import SiteGeoDaoRedis
+from redisolar.dao.redis import CapacityReportDaoRedis
 from redisolar.models import Coordinate
 from redisolar.models import GeoQuery
 from redisolar.models import GeoUnit
+from redisolar.models import MeterReading
 from redisolar.models import Site
 
 PORTLAND = Coordinate(lat=45.523064, lng=-122.676483)
@@ -13,6 +17,11 @@ BEAVERTON = Coordinate(lat=45.485168, lng=-122.804489)
 @pytest.fixture
 def site_geo_dao(redis, key_schema):
     yield SiteGeoDaoRedis(redis, key_schema)
+
+
+@pytest.fixture
+def capacity_dao(redis, key_schema):
+    yield CapacityReportDaoRedis(redis, key_schema)
 
 
 def test_insert(redis, site_geo_dao):
@@ -144,3 +153,50 @@ def test_find_sites_by_geo(site_geo_dao):
     site_geo_dao.insert_many(site1, site2, site3)
     query = GeoQuery(coordinate=PORTLAND, radius=1, radius_unit=GeoUnit.MI)
     assert site_geo_dao.find_by_geo(query) == {site1, site2}
+
+
+@pytest.mark.skip("Remove for challenge #5")
+def test_find_by_geo_with_excess_capacity(site_geo_dao, capacity_dao):
+    site1 = Site(id=1,
+                 capacity=10.0,
+                 panels=100,
+                 address="100 SE Pine St.",
+                 city="Portland",
+                 state="OR",
+                 postal_code="97202",
+                 coordinate=PORTLAND)
+
+    site_geo_dao.insert(site1)
+
+    # Check that this site is returned when we're not looking for excess capacity.
+    query = GeoQuery(coordinate=PORTLAND, radius=1, radius_unit=GeoUnit.MI)
+    assert site_geo_dao.find_by_geo(query) == {site1}
+
+    # Simulate changing a meter reading with no excess capacity.
+    reading = MeterReading(site_id=site1.id,
+                           wh_used=1.0,
+                           wh_generated=0.0,
+                           temp_c=10,
+                           timestamp=datetime.datetime.now())
+    capacity_dao.update(reading)
+
+    # In this case, no sites are returned for an excess capacity query.
+    query = GeoQuery(coordinate=PORTLAND,
+                     radius=1,
+                     radius_unit=GeoUnit.MI,
+                     only_excess_capacity=True)
+    sites = site_geo_dao.find_by_geo(query)
+    assert len(sites) == 0
+
+
+    # Simulate changing a meter reading indicating excess capacity.
+    reading = MeterReading(site_id=site1.id,
+                           wh_used=1.0,
+                           wh_generated=2.0,
+                           temp_c=10,
+                           timestamp=datetime.datetime.now())
+    capacity_dao.update(reading)
+
+    # In this case, one site is returned on the excess capacity query
+    sites = site_geo_dao.find_by_geo(query)
+    assert site1 in sites
