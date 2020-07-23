cfb5c4e0f8828f5d3233442247e714bf4f3d2410
Andrew Brookins <andrew.brookins@redislabs.com>
2020-07-23 15:43:10 -0700

    Cleanup for timeseries challenge

--- redisolar/dao/redis/meter_reading.py
+++ redisolar/dao/redis/meter_reading.py
@@ -1,11 +1,12 @@
-from redisolar.models import MeterReading
 from redisolar.dao.base import MeterReadingDaoBase
+from redisolar.dao.redis.base import RedisDaoBase
 from redisolar.dao.redis.capacity_report import CapacityReportDaoRedis
 from redisolar.dao.redis.feed import FeedDaoRedis
 from redisolar.dao.redis.metric import MetricDaoRedis
+from redisolar.models import MeterReading
+
 # Uncomment for Challenge #3
-#from redisolar.dao.redis.site_stats import SiteStatsDaoRedis
-from redisolar.dao.redis.base import RedisDaoBase
+# from redisolar.dao.redis import SiteStatsDaoRedis
 
 
 class MeterReadingDaoRedis(MeterReadingDaoBase, RedisDaoBase):
@@ -16,4 +17,4 @@ class MeterReadingDaoRedis(MeterReadingDaoBase, RedisDaoBase):
         FeedDaoRedis(self.redis, self.key_schema).insert(meter_reading, **kwargs)
 
         # Uncomment for Challenge #3
-        #SiteStatsDaoRedis(self.redis, self.key_schema).update(meter_reading, **kwargs)
+        # SiteStatsDaoRedis(self.redis, self.key_schema).update(meter_reading, **kwargs)
